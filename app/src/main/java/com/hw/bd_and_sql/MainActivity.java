0d08f9067749270b346e6fab65b9d12cdcda81fe
Amit <aamitadler@gmail.com>
2025-03-01 20:43:15 +0200

    the view list is buggy but it works

--- app/src/main/java/com/hw/bd_and_sql/MainActivity.java
+++ app/src/main/java/com/hw/bd_and_sql/MainActivity.java
@@ -1,10 +1,13 @@
 package com.hw.bd_and_sql;
 
+import static com.hw.bd_and_sql.Users.KEY_ID;
 import static com.hw.bd_and_sql.Users.TABLE_USERS;
 import static com.hw.bd_and_sql.User.*;
 
 import android.app.Activity;
+import android.app.usage.UsageEventsQuery;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -22,9 +25,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    Button addPerson;
-    Button removePerson;
-    Button editPerson;
+    Button btnAddPerson;
+    Button btnRemovePerson;
+    Button btnEditPerson;
     EditText etId, etName, etAge, etPass;
     ListView listViewUsers;
 
@@ -42,14 +45,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
-        addPerson.setOnClickListener(v -> addUser());
+        btnAddPerson.setOnClickListener(v -> addUser());
+        btnRemovePerson.setOnClickListener(v -> removeUser());
+        btnEditPerson.setOnClickListener(v -> editUser());
     }
+
 
     void init()
     {
-        addPerson = findViewById(R.id.btnAddUser);
-        removePerson = findViewById(R.id.btnRemove);
-        editPerson = findViewById(R.id.btnEdit);
+        btnAddPerson = findViewById(R.id.btnAddUser);
+        btnRemovePerson = findViewById(R.id.btnRemove);
+        btnEditPerson= findViewById(R.id.btnEdit);
         etId=findViewById(R.id.etId);
         etName=findViewById(R.id.etName);
         etAge=findViewById(R.id.etAge);
@@ -88,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
         ((UserAdapter) listViewUsers.getAdapter()).notifyDataSetChanged();
     }
 
+
     void addUser()
     {
         String name, pass, strAge;
@@ -123,5 +130,85 @@ public class MainActivity extends AppCompatActivity {
 
             ((UserAdapter) listViewUsers.getAdapter()).notifyDataSetChanged();
         }
+    }
+    void removeUser()
+    {
+        db = hlp.getWritableDatabase();
+        String str = etId.getText().toString();
+        if(str.isEmpty())
+        {
+            return;
+        }
+        int rows = db.delete(TABLE_USERS, KEY_ID+"=?", new String[] {str});
+        if(rows == 0)
+            Toast.makeText(this, "No user found", Toast.LENGTH_SHORT).show();
+        else {
+            users.remove(Integer.parseInt(str) - 1);
+        }
+        db.close();
+        updateDB();
+
+    }
+    void editUser()
+    {
+        String str = etId.getText().toString();
+        if(str.isEmpty())
+        {
+            return;
+        }
+
+        int
+                userId = Integer.parseInt(str),
+                newAge;
+        String
+                newName = etName.getText().toString(),
+                newPass = etAge.getText().toString(),
+                newAgeStr = etAge.getText().toString();
+
+        if (newName.isEmpty() || newPass.isEmpty() || newAgeStr.isEmpty()) {
+            Toast.makeText(this, "One of the fields is missing!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        newAge = Integer.parseInt(newAgeStr);
+
+        ContentValues cv = new ContentValues();
+        cv.put(Users.NAME, newName);
+        cv.put(Users.AGE, newAge);
+        cv.put(Users.PASSWORD, newPass);
+
+        db = hlp.getWritableDatabase();
+        int rowsUpdated = db.update(TABLE_USERS, cv, Users.KEY_ID+"=?", new String[]{String.valueOf(userId)});
+        db.close();
+        if (rowsUpdated > 0)
+        {
+            Toast.makeText(this, "User updated successfully", Toast.LENGTH_LONG).show();
+            updateDB();
+        } else {
+            Toast.makeText(this, "No user found with that ID", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void updateDB()
+    {
+        db = hlp.getReadableDatabase();
+        Cursor crsr = db.query(TABLE_USERS,null,null,null,null,null,null);
+        int colKEY_ID = crsr.getColumnIndex(Users.KEY_ID);
+        int colName = crsr.getColumnIndex(Users.NAME);
+        int colAge = crsr.getColumnIndex(Users.AGE);
+        int colPass = crsr.getColumnIndex(Users.PASSWORD);
+        crsr.moveToFirst();
+        while (!crsr.isAfterLast())
+        {
+            tmpUser = new User();
+            tmpUser.setKey_id(crsr.getInt(colKEY_ID));
+            tmpUser.setName(crsr.getString(colName));
+            tmpUser.setAge(crsr.getInt(colAge));
+            tmpUser.setPassword(crsr.getString(colPass));
+            users.add(tmpUser);
+            crsr.moveToNext();
+        }
+        crsr.close();
+        db.close();
+        ((UserAdapter) listViewUsers.getAdapter()).notifyDataSetChanged();
     }
 }
